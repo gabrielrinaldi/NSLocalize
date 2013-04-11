3078dffdaf22ac819b4f92efa64d63b95665b83f
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2013-04-11 15:39:59 -0700

    Using shoulda

---
+++ test/helper.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'bundler'
+
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts %Q\Run "bundle install" to install missing gems\
+  exit e.status_code
+end
+
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'nslocalize'
+
+class Test::Unit::TestCase
+
+end

e9082e94e3b9b6c886eaf1c3afb4eaebdba89ad0
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2013-04-11 16:24:30 -0700

    Removed unused code

--- lib/nslocalize.rb
+++ lib/nslocalize.rb
@@ -1,16 +1,7 @@
-# This class is responsible for parsing all strings in a given text or file.
 require 'nslocalize/export'
 require 'nslocalize/parse'
-require 'nslocalize/version'
 
+# This module is responsible for parsing all strings in a given text or file.
 module NSLocalize
-  USER_AGENT = "nslocalize-gem/#{NSLocalize::VERSION} (#{RUBY_PLATFORM}) ruby/#{RUBY_VERSION}"
 
-  def self.user_agent
-    @@user_agent ||= USER_AGENT
-  end
-
-  def self.user_agent=(agent)
-    @@user_agent = agent
-  end
 end
